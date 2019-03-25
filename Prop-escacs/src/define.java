5631c6939e5ab67595f994e52867f4847afbab70
marc1222 <marcgp12@gmail.com>
2019-03-25 23:04:40 +0100

    class exception and define

---
+++ Prop-escacs/src/define.java
@@ -0,0 +1,22 @@
+public class define {
+
+    //juegador
+    public static final int WHITE = 0;
+    public static final int BLACK = 1;
+
+    //peça
+    public static final int ALFIL = 2;
+    public static final int TORRE = 3;
+    public static final int REI = 4;
+    public static final int REINA = 5;
+    public static final int PEO = 6;
+    public static final int PECA_NULA = 7;
+
+    //si es necessiten mes, afegiu
+
+
+
+
+
+
+}
