2794f8cccdc89b019ebd6df99c5fd2096368f8d5
narcis.rodas <narcis.rodas@est.fib.upc.edu>
2019-04-07 11:56:17 +0200

    Main

--- Prop-escacs/src/Main.java
+++ Prop-escacs/src/Main.java
@@ -84,12 +84,27 @@ public class Main {
             } while ((val > 5 || val < 1));
             switch (val) {
                 case 1: {
-                    //String[][] problemes = consultarProblemes();
+                    String[][] problemes = consultarProblemes();
+                    for (int i = 0; i < problemes.length(); ++i) {
+                        System.out.println(problemes[i][0] + " " + problemes[i][1] + " " problemes[i][2] + " "
+                        + problemes[i][3] + " " + problemes[i][4]);
+                    }
+                    Scanner sc = new Scanner(System.in);
+                    int op = 0;
+                    boolean primer = true;
+                    while (op < 1 || op > (problemes.length() - 1)) {
+                        if (!primer) System.out.println("Error");
+                        else primer = false;
+                        System.out.println("Selecciona un problema");
+                        op = sc.nextInt();
+                    }
 
                     // fer bucles per mostar problemes
 
-                    //p = new Problema();
-                    //getProblemaId(problemes[val-1][0], p);
+                    Problema p = new Problema();
+                    int res = getProblemaId(problemes[op][0], p);
+                    if (res < 1) ; //
+
 
 
                     // seleccionar oponent: maquina o usuari
@@ -102,14 +117,80 @@ public class Main {
                     break;
                 }
                 case 2: {
-                    // p = new Problema();
-                    //scanejar jugades i fen
-                    // p.crearProblema(jugades, fen)
-                    // cancelar
-                    //afegir
+                    int res;
+                    boolean primer = true;
+                    do {
+                        Scanner sc = new Scanner(System.in);
+                        if (!primer) {
+                            System.out.println("Error, torna a introduir el problema o -1 per sortir");
+                            if (sc.nextInt() == -1) break;
+                        }
+                        else primer = false;
+                        System.out.println("Escriu el nombre de jugades del problema");
+                        int njug = 0;
+                        boolean prim = true;
+                        while (njug < 1) {
+                            if (!prim) System.out.println("Error");
+                            else prim = false;
+                            njug = sc.nextInt();
+                        }
+                        System.out.println("Escriu el FEN del problema");
+                        Scanner scs = new Scanner(System.in).useDelimiter(System.lineSeparator());
+                        String fen;
+                        fen = scs.next();
+                        Problema p = new Problema();
+                        //scanejar jugades i fen
+                        res = p.crearProblema(njug, fen);
+                        // cancelar
+                        //afegir
+                    } while (res < 0);
                     break;
                 }
                 case 3: {
+                    int res;
+                    boolean prim = true;
+                    String[][] problemes = consultarProblemes();
+                    do {
+                        Scanner sc = new Scanner(System.in);
+                        if (!prim) {
+                            System.out.println("Error, torna a introduir el nou problema o -1 per sortir");
+                            if (sc.nextInt() == -1) break;
+                        }
+                        else prim = false;
+                        for (int i = 0; i < problemes.length(); ++i) {
+                            System.out.println(problemes[i][0] + " " + problemes[i][1] + " " problemes[i][2] + " "
+                                    + problemes[i][3] + " " + problemes[i][4]);
+                        }
+                        int op = 0;
+                        boolean primer = true;
+                        while (op < 1 || op > (problemes.length() - 1)) {
+                            if (!primer) System.out.println("Error");
+                            else primer = false;
+                            System.out.println("Selecciona un problema");
+                            op = sc.nextInt();
+                        }
+                        Problema p = new Problema();
+                        int res = getProblemaId(problemes[op][0], p);
+                        if (res < 1) ;
+                        System.out.println("Escriu el nou nombre de jugades del problema");
+                        int njug = 0;
+                        boolean prim = true;
+                        while (njug < 1) {
+                            if (!prim) System.out.println("Error");
+                            else prim = false;
+                            njug = sc.nextInt();
+                        }
+                        if (p.getPrimer() == define.WHITE) {
+                            System.out.println("El fen actual és: " + p.getPosIni() + " w - - 0 1");
+                        }
+                        else System.out.println("El fen actual és: " + p.getPosIni() + " b - - 0 1");
+                        System.out.println("Escriu el nou FEN del problema");
+                        Scanner scs = new Scanner(System.in).useDelimiter(System.lineSeparator());
+                        String fen;
+                        fen = scs.next();
+                        res = p.modificar_problema(fen, njug);
+                    } while (res < 0);
+
                     // consultar problemes
                     // seleccionar problema
                     // p = getProblemaId
@@ -118,6 +199,25 @@ public class Main {
                     break;
                 }
                 case 4: {
+                    String[][] problemes = consultarProblemes();
+                    for (int i = 0; i < problemes.length(); ++i) {
+                        System.out.println(problemes[i][0] + " " + problemes[i][1] + " " problemes[i][2] + " "
+                                + problemes[i][3] + " " + problemes[i][4]);
+                    }
+                    Scanner sc = new Scanner(System.in);
+                    int op = 0;
+                    boolean primer = true;
+                    while (op < 1 || op > (problemes.length() - 1)) {
+                        if (!primer) System.out.println("Error");
+                        else primer = false;
+                        System.out.println("Selecciona un problema");
+                        op = sc.nextInt();
+                    }
+
+                    Problema p = new Problema();
+                    int res = getProblemaId(problemes[op][0], p);
+                    if (res < 1) ; //
+                    p.eliminar_problema(); //errors?
                     // consultar problemes
                     // p = getProblemaId
                     // p.eliminarProblema()
